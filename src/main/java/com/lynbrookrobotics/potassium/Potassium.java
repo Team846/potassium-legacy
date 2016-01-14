b8be262acd503054147ae3ad0e5efc42513d20cb
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-01-13 19:56:51 -0800

    Actually add the Potassium class

---
+++ src/main/java/com/lynbrookrobotics/potassium/Potassium.java
@@ -0,0 +1,16 @@
+package com.lynbrookrobotics.potassium;
+
+import com.lynbrookrobotics.potassium.components.Component;
+import com.lynbrookrobotics.potassium.events.Event;
+import com.lynbrookrobotics.potassium.tasks.Task;
+
+public class Potassium {
+    /**
+     * Updates (in order): events, tasks, and components
+     */
+    public static void updateAll() {
+        Event.updateEvents();
+        Task.updateCurrentTask();
+        Component.updateComponents();
+    }
+}
