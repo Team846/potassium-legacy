8f3a0a4d00afd555e29b3e5655c9c5d5afae551b
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-01-15 19:26:34 -0800

    Add event that triggers based on game state

---
+++ src/main/java/com/lynbrookrobotics/potassium/defaults/events/InGameState.java
@@ -0,0 +1,31 @@
+package com.lynbrookrobotics.potassium.defaults.events;
+
+import com.lynbrookrobotics.potassium.events.SteadyEvent;
+import edu.wpi.first.wpilibj.DriverStation;
+
+public class InGameState extends SteadyEvent {
+    public enum GameState {
+        DISABLED,
+        AUTONOMOUS,
+        ENABLED
+    }
+
+    DriverStation station;
+    GameState expectedState;
+
+    public InGameState(DriverStation station, GameState expectedState) {
+        this.station = station;
+        this.expectedState = expectedState;
+    }
+
+    @Override
+    protected void checkForTrigger() {
+        if (expectedState == GameState.DISABLED && station.isDisabled()) {
+            trigger();
+        } else if (expectedState == GameState.AUTONOMOUS && station.isAutonomous()) {
+            trigger();
+        } else if (expectedState == GameState.ENABLED && station.isEnabled()) {
+            trigger();
+        }
+    }
+}
