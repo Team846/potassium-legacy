6367c61112db2bea0e032b90a5b908759d2614f1
Sanghak (Andy) Chun <sanghakchun@gmail.com>
2016-02-14 17:08:45 -0800

    Removed static booleans

--- src/main/java/com/lynbrookrobotics/potassium/defaults/events/ButtonPress.java
+++ src/main/java/com/lynbrookrobotics/potassium/defaults/events/ButtonPress.java
@@ -4,9 +4,6 @@ import com.lynbrookrobotics.potassium.events.ImpulseEvent;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class ButtonPress extends ImpulseEvent {
-    private static final boolean BUTTON_HELD = true;
-    private static final boolean BUTTON_NOT_HELD = false;
-
     Joystick joystick;
     int button;
     boolean lastState;
@@ -22,7 +19,7 @@ public class ButtonPress extends ImpulseEvent {
     protected void checkForTrigger() {
         boolean currentState = joystick.getRawButton(this.button);
 
-        if (this.lastState == BUTTON_NOT_HELD && currentState == BUTTON_HELD) {
+        if (!this.lastState && currentState) {
             trigger();
         }
 
